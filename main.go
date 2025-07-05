4f4326d1958ea563c9a9cae60a0c332f8ea94fc5
kyomel <michaellapandio04@gmail.com>
2025-07-05 21:18:40 +0700

    feat: add file validation and overwrite confirmation for input/output paths

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -24,10 +26,53 @@ func main() {
 		os.Exit(0)
 	}
 
+	if err := validateInput(inputPath); err != nil {
+		fmt.Printf("invalid input: %s \n", err)
+		os.Exit(0)
+	}
+
+	if err := validateOutput(outputPath); err != nil {
+		fmt.Printf("invalid output: %s \n", err)
+		os.Exit(0)
+	}
 }
 
 func printUsage() {
 	fmt.Println("Usage: mockdata [-i || --input] <input file> [-o || --output] <output file>")
 	fmt.Println("-i --input: File input berupa JSON sebagai template")
 	fmt.Println("-o --output: File output berupa JSON sebagai hasil")
+}
+
+// cek apakah file input tersebut ada atau tidak
+func validateInput(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return err
+	}
+
+	return nil
+}
+
+// cek apakah file output tersebut ada atau tidak
+func validateOutput(path string) error {
+	if _, err := os.Stat(path); os.IsExist(err) {
+		return nil
+	}
+
+	fmt.Println("File sudah ada di lokasi")
+
+	confirmOverwrite()
+	return nil
+}
+
+func confirmOverwrite() {
+	fmt.Println("Apakah anda ingin menimpa file yang sudah ada (y/t)")
+
+	reader := bufio.NewReader(os.Stdin)
+	response, _ := reader.ReadString('\n')
+	response = strings.ToLower(strings.TrimSpace(response))
+
+	if response != "y" && response != "yes" && response != "ya" {
+		fmt.Println("Membatalkan proses...")
+		os.Exit(0)
+	}
 }
