2a9a9b570793b2f9d5691e88a1dad02cb481054d
kyomel <michaellapandio04@gmail.com>
2025-08-13 00:42:37 +0700

    feat: add output generation and JSON file writing functionality

--- main.go
+++ main.go
@@ -49,6 +49,17 @@ func main() {
 		fmt.Printf("gagal memvalidasi data: %s \n", err.Error())
 		os.Exit(0)
 	}
+
+	result, err := generateOutput(mapping)
+	if err != nil {
+		fmt.Printf("gagal membuat data: %s \n", err)
+		os.Exit(0)
+	}
+
+	if err := writeOutput(outputPath, result); err != nil {
+		fmt.Printf("gagal menulis hasil: %s \n", err)
+		os.Exit(0)
+	}
 }
 
 func printUsage() {
@@ -137,6 +148,42 @@ func validateType(mapping map[string]string) error {
 		if !supported[value] {
 			return errors.New("tipe data tidak didukung")
 		}
+	}
+
+	return nil
+}
+
+func generateOutput(mapping map[string]string) (map[string]any, error) {
+	result := make(map[string]any)
+
+	for key, dataType := range mapping {
+		result[key] = fmt.Sprintf("%s palsu", dataType)
+	}
+
+	return result, nil
+}
+
+func writeOutput(path string, result map[string]any) error {
+	if path == " " {
+		return errors.New("path tidak valid")
+	}
+
+	flags := os.O_RDWR | os.O_CREATE | os.O_TRUNC
+
+	file, err := os.OpenFile(path, flags, 0644)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	resultByte, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	if _, err := file.Write(resultByte); err != nil {
+		return err
 	}
 
 	return nil
